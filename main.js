85f66d6feccb328799615194fa25edb2367a94c1
Ashu Boris <borisashu5@gmail.com>
2024-12-23 16:09:32 +0100

    Made changes to the loading animation

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 function hidePreloader() {
+  setTimeout(() => {
+    const preloader = document.getElementById('preloader');
+    preloader.style.opacity = '0';
+    preloader.style.transition = 'opacity 0.5s ease';
+    
     setTimeout(() => {
-      const preloader = document.getElementById('preloader');
       preloader.style.display = 'none';
-      
       const mainContent = document.getElementById('main-content');
       mainContent.style.display = 'block';
-    }, 10000); // 10000 milliseconds = 10 seconds
-  }
-  
+    }, 500);
+  }, 10000);
+}
